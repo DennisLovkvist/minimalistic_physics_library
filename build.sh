9bdc9924f194615c8f323f790325398e9b34d3ef
Dennis Lövkvist <dennis.lovkvist@protonmail.com>
2022-08-29 00:17:11 +0200

    removed copying of textures folder

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 mkdir -p bin/debug/
 cp -u shaders bin/debug/ -d -r
-cp -u textures bin/debug/ -d -r
 gcc -o bin/debug/hazelholm_c stb_image.c delo2d.c mpl.c mpl_debug.c game.c main.c -Ilibs -lglfw -lGLEW -lGL -msse2 -lm -ldl -O2
 ./bin/debug/hazelholm_c
